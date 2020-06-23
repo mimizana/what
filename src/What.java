7ae4872a89d0fc622ed1b01b0431809d3344fd78
mimizano@gmail.com <mimizano@gmail.com>
2020-06-23 11:59:06 -0400

    second

--- src/What.java
+++ src/What.java
@@ -13,22 +13,22 @@ public class What {
 
         System.out.print("Second number? : ");
         num2 = keyboard.nextInt();
-        double sum = num1+ num2;
-        double product = num1 *num2;
-        double av = num1 * num2/2;
-        if ( sum >=200){
-            System.out.println("Sum of the numbers : " +sum +"*,"+ "product :" + product+ " average: "+ av );
+        double sum = num1 + num2;
+        double product = num1 * num2;
+        double av = num1 * num2 / 2;
+        if (sum >= 200) {
+            System.out.println("Sum of the numbers : " + sum + "*," + "product :" + product + " average: " + av);
 
         }
-        if (sum>=1000){
-            System.out.println("Sum of the numbers : " +sum +" ~," +"product :" + product+ " average: "+ av );
+        if (sum <= 1000 && sum >= 200) {
+            System.out.println("Sum of the numbers : " + sum + " ~," + "product :" + product + " average: " + av);
         }
-        else {
-            System.out.println("Sum of the numbers : " +sum + "product :" + product+ " average: "+ av );
+        else{
+            System.out.println("Sum of the numbers : " + sum + "product :" + product + " average: " + av);
         }
     }
-
 }
+
 //Start with the code below and create a program that will output the sum,
 // product, and average of num1 and num2.
 // If the calculated sum is over 200, print an asterisk next to the sum.
